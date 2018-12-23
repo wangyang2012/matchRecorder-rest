745a12537d8b4d10c754b420ad810c6f726e3c35
wangyang2012 <wangyang1712@gmail.com>
2018-12-23 04:17:02 +0100

    sql

--- src/main/resources/sql/match_recorder.sql
+++ src/main/resources/sql/match_recorder.sql
@@ -18,13 +18,13 @@ USE `match_recorder`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `match`
+-- Table structure for table `matches`
 --
 
-DROP TABLE IF EXISTS `match`;
+DROP TABLE IF EXISTS `matches`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `match` (
+CREATE TABLE `matches` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `player_1` int(11) NOT NULL,
   `player_2` int(11) NOT NULL,
@@ -41,21 +41,21 @@ CREATE TABLE `match` (
   `sub_score_1_5` int(11) DEFAULT NULL,
   `sub_score_2_5` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `pk_match_player_1_idx` (`player_1`),
   KEY `pk_match_player_2_idx` (`player_2`),
   CONSTRAINT `pk_match_player_1` FOREIGN KEY (`player_1`) REFERENCES `player` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `pk_match_player_2` FOREIGN KEY (`player_2`) REFERENCES `player` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `match`
+-- Dumping data for table `matches`
 --
 
-LOCK TABLES `match` WRITE;
-/*!40000 ALTER TABLE `match` DISABLE KEYS */;
-/*!40000 ALTER TABLE `match` ENABLE KEYS */;
+LOCK TABLES `matches` WRITE;
+/*!40000 ALTER TABLE `matches` DISABLE KEYS */;
+INSERT INTO `matches` VALUES (1,1,2,2,3,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(2,1,2,3,2,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(3,1,2,3,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `matches` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -68,9 +68,8 @@ DROP TABLE IF EXISTS `player`;
 CREATE TABLE `player` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -79,6 +78,7 @@ CREATE TABLE `player` (
 
 LOCK TABLES `player` WRITE;
 /*!40000 ALTER TABLE `player` DISABLE KEYS */;
+INSERT INTO `player` VALUES (1,'Yang'),(2,'Test'),(3,'ttt'),(4,'aaa'),(5,'aaa'),(6,'aaa'),(7,'xxx'),(8,'yy');
 /*!40000 ALTER TABLE `player` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -91,4 +91,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-23  1:10:25
+-- Dump completed on 2018-12-23  4:16:03
